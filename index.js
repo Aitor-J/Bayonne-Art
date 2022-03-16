43c9357a9b6e0626eb000584ec8e795b9ffc1145
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-03-16 14:32:23 +0100

    mosaique is done

---
+++ index.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll(".mosaique > div");
+console.log(boxes);
+box1.addEventListener("mouseover", function() {
+document.getElementById("title").style.opacity = "1"; 
+});
+box1.addEventListener("mouseout", function() {
+document.getElementById("title").style.opacity = "0"; 
+});
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("mouseover", function() {
+    boxes[i].querySelector("a h3").style.opacity = "1"
+ });
+    boxes[i].addEventListener("mouseout", function() { 
+    boxes[i].querySelector("a h3").style.opacity = "0"
+});
+}
